27800048031ad651b07c35047a3223b3e56e34a9
CtheSky <cthesky@yeah.net>
2016-11-21 22:14:27 +0800

    feat: add left/right flips

--- js/ConstructBlock.js
+++ js/ConstructBlock.js
@@ -17,6 +17,7 @@ var   b2Vec2 = Box2D.Common.Math.b2Vec2
     , b2FixtureDef = Box2D.Dynamics.b2FixtureDef
     , b2Fixture = Box2D.Dynamics.b2Fixture
     , b2World = Box2D.Dynamics.b2World
+    , b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef
     , b2MassData = Box2D.Collision.Shapes.b2MassData
     , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
     , b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
@@ -81,6 +82,20 @@ DynamicTriangle.prototype = Object.create(Draggable.prototype);
 DynamicTriangle.prototype.constructor = DynamicTriangle;
 DynamicTriangle.prototype.createBox2dObjectInWorld = createTriangleBox2dObjectInWorld;
 
+var LeftFlip = function(imageProp) {
+    Draggable.call(this, DYNAMIC_DEF_EXAMPLE, imageProp);
+};
+LeftFlip.prototype = Object.create(Draggable.prototype);
+LeftFlip.prototype.constructor = LeftFlip;
+LeftFlip.prototype.createBox2dObjectInWorld = createLeftFlipBox2dObjectInWorld;
+
+var RightFlip = function(imageProp) {
+    Draggable.call(this, DYNAMIC_DEF_EXAMPLE, imageProp);
+};
+RightFlip.prototype = Object.create(Draggable.prototype);
+RightFlip.prototype.constructor = RightFlip;
+RightFlip.prototype.createBox2dObjectInWorld = createRightFlipBox2dObjectInWorld;
+
 // Helper function to create square box2d object
 function createSquareBox2dObjectInWorld(world) {
     this.b2FixtureDef.shape = new b2PolygonShape;
@@ -96,7 +111,7 @@ function createSquareBox2dObjectInWorld(world) {
 }
 
 // Helper function to create circle box2d object
-function createCircleBox2dObjectInWorld(world){
+function createCircleBox2dObjectInWorld(world) {
     this.b2FixtureDef.shape = new b2CircleShape(this.imageWidth * 0.5 / SCALE);
 
     this.b2BodyDef.position.x = (this.imageX + this.imageWidth * 0.5) / SCALE;
@@ -106,7 +121,7 @@ function createCircleBox2dObjectInWorld(world){
 }
 
 // Helper function to create triangle box2d object
-function createTriangleBox2dObjectInWorld(world){
+function createTriangleBox2dObjectInWorld(world) {
     var vectors = [
         new b2Vec2(-0.5 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
         new b2Vec2(0.5 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
@@ -121,4 +136,116 @@ function createTriangleBox2dObjectInWorld(world){
     var vecToCenter = new b2Vec2((this.imageX + 0.5 * this.imageWidth) / SCALE, (this.imageY + 0.5 * this.imageHeight) / SCALE);
     body.SetTransform(new b2Transform(vecToCenter, b2Mat22.FromAngle(this.imageDegree * TO_RADIANS)));
     body.CreateFixture(this.b2FixtureDef);
+}
+
+// Help function to create left flip box2d object
+function createLeftFlipBox2dObjectInWorld(world) {
+    // ----------- create flip body ---------
+    var vectors = [
+        new b2Vec2(-0.5 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
+        new b2Vec2(0.5 * this.imageWidth / SCALE, -0.4 * this.imageHeight / SCALE),
+        new b2Vec2(0.5 * this.imageWidth / SCALE, -0.3 * this.imageHeight / SCALE),
+        new b2Vec2(-0.5 * this.imageWidth / SCALE, -0.1 * this.imageHeight / SCALE)
+    ];
+    this.b2FixtureDef.shape = new b2PolygonShape;
+    this.b2FixtureDef.shape.SetAsArray(vectors, vectors.length);
+
+    this.b2BodyDef.position.x = (this.imageX + this.imageWidth * 0.5) / SCALE;
+    this.b2BodyDef.position.y = (this.imageY + this.imageHeight * 0.5) / SCALE;
+
+    var body = world.CreateBody(this.b2BodyDef);
+    var vecToCenter = new b2Vec2((this.imageX + 0.5 * this.imageWidth) / SCALE, (this.imageY + 0.5 * this.imageHeight) / SCALE);
+    body.SetTransform(new b2Transform(vecToCenter, b2Mat22.FromAngle(this.imageDegree * TO_RADIANS)));
+    body.CreateFixture(this.b2FixtureDef);
+
+    // --------- create static block to joint -----
+    vectors = [
+        new b2Vec2(-0.5 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
+        new b2Vec2(-0.49 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
+        new b2Vec2(-0.49 * this.imageWidth / SCALE, -0.49 * this.imageHeight / SCALE),
+        new b2Vec2(-0.5 * this.imageWidth / SCALE, -0.49 * this.imageHeight / SCALE)
+    ];
+    this.b2BodyDef = STATIC_DEF_EXAMPLE.bodyDef;
+    this.b2FixtureDef.shape = new b2PolygonShape;
+    this.b2FixtureDef.shape.SetAsArray(vectors, vectors.length);
+    this.b2BodyDef.position.x = (this.imageX + this.imageWidth * 0.5) / SCALE;
+    this.b2BodyDef.position.y = (this.imageY + this.imageHeight * 0.5) / SCALE;
+    var body2 = world.CreateBody(this.b2BodyDef);
+    vecToCenter = new b2Vec2((this.imageX + 0.5 * this.imageWidth) / SCALE, (this.imageY + 0.5 * this.imageHeight) / SCALE);
+    body2.SetTransform(new b2Transform(vecToCenter, b2Mat22.FromAngle(this.imageDegree * TO_RADIANS)));
+    body2.CreateFixture(this.b2FixtureDef);
+
+    // -------- create joint ---------
+    var jointDef = new b2RevoluteJointDef();
+    // compute joint position
+    var vLength = Math.sqrt(this.imageWidth * this.imageWidth / 4 + this.imageHeight * this.imageHeight / 4) / SCALE;
+    var degreeOffset = 180 - Math.atan(this.imageHeight / this.imageWidth) / TO_RADIANS;
+    var vRadian = (degreeOffset - this.imageDegree) * TO_RADIANS;
+    var vectorToAdd = new b2Vec2(vLength * Math.cos(vRadian), -1 * vLength * Math.sin(vRadian));
+    var vectorPosition = body2.GetPosition().Copy();
+    vectorPosition.Add(vectorToAdd);
+    jointDef.Initialize(body, body2, vectorPosition);
+    jointDef.lowerAngle     = -30 * TO_RADIANS;
+    jointDef.upperAngle     = 30 * TO_RADIANS;
+    jointDef.enableLimit    = true;
+    // jointDef.maxMotorTorque = 10.0;
+    // jointDef.motorSpeed     = 2.0;
+    // jointDef.enableMotor    = true;
+    world.CreateJoint(jointDef);
+}
+
+// Help function to create left flip box2d object
+function createRightFlipBox2dObjectInWorld(world) {
+    // ----------- create flip body ---------
+    var vectors = [
+        new b2Vec2(-0.5 * this.imageWidth / SCALE, -0.4 * this.imageHeight / SCALE),
+        new b2Vec2(0.5 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
+        new b2Vec2(0.5 * this.imageWidth / SCALE, -0.1 * this.imageHeight / SCALE),
+        new b2Vec2(-0.5 * this.imageWidth / SCALE, -0.3 * this.imageHeight / SCALE)
+    ];
+    this.b2FixtureDef.shape = new b2PolygonShape;
+    this.b2FixtureDef.shape.SetAsArray(vectors, vectors.length);
+
+    this.b2BodyDef.position.x = (this.imageX + this.imageWidth * 0.5) / SCALE;
+    this.b2BodyDef.position.y = (this.imageY + this.imageHeight * 0.5) / SCALE;
+
+    var body = world.CreateBody(this.b2BodyDef);
+    var vecToCenter = new b2Vec2((this.imageX + 0.5 * this.imageWidth) / SCALE, (this.imageY + 0.5 * this.imageHeight) / SCALE);
+    body.SetTransform(new b2Transform(vecToCenter, b2Mat22.FromAngle(this.imageDegree * TO_RADIANS)));
+    body.CreateFixture(this.b2FixtureDef);
+
+    // --------- create static block to joint -----
+    vectors = [
+        new b2Vec2(0.49 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
+        new b2Vec2(0.5 * this.imageWidth / SCALE, -0.5 * this.imageHeight / SCALE),
+        new b2Vec2(0.5 * this.imageWidth / SCALE, -0.49 * this.imageHeight / SCALE),
+        new b2Vec2(0.49 * this.imageWidth / SCALE, -0.49 * this.imageHeight / SCALE)
+    ];
+    this.b2BodyDef = STATIC_DEF_EXAMPLE.bodyDef;
+    this.b2FixtureDef.shape = new b2PolygonShape;
+    this.b2FixtureDef.shape.SetAsArray(vectors, vectors.length);
+    this.b2BodyDef.position.x = (this.imageX + this.imageWidth * 0.5) / SCALE;
+    this.b2BodyDef.position.y = (this.imageY + this.imageHeight * 0.5) / SCALE;
+    var body2 = world.CreateBody(this.b2BodyDef);
+    vecToCenter = new b2Vec2((this.imageX + 0.5 * this.imageWidth) / SCALE, (this.imageY + 0.5 * this.imageHeight) / SCALE);
+    body2.SetTransform(new b2Transform(vecToCenter, b2Mat22.FromAngle(this.imageDegree * TO_RADIANS)));
+    body2.CreateFixture(this.b2FixtureDef);
+
+    // -------- create joint ---------
+    var jointDef = new b2RevoluteJointDef();
+    // compute joint position
+    var vLength = Math.sqrt(this.imageWidth * this.imageWidth / 4 + this.imageHeight * this.imageHeight / 4) / SCALE;
+    var degreeOffset = Math.atan(this.imageHeight / this.imageWidth) / TO_RADIANS;
+    var vRadian = (degreeOffset - this.imageDegree) * TO_RADIANS;
+    var vectorToAdd = new b2Vec2(vLength * Math.cos(vRadian), -1 * vLength * Math.sin(vRadian));
+    var vectorPosition = body2.GetPosition().Copy();
+    vectorPosition.Add(vectorToAdd);
+    jointDef.Initialize(body, body2, vectorPosition);
+    jointDef.lowerAngle     = -30 * TO_RADIANS;
+    jointDef.upperAngle     = 30 * TO_RADIANS;
+    jointDef.enableLimit    = true;
+    // jointDef.maxMotorTorque = 10.0;
+    // jointDef.motorSpeed     = 2.0;
+    // jointDef.enableMotor    = true;
+    world.CreateJoint(jointDef);
 }
